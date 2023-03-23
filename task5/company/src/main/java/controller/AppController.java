2034600023ce9804c449960066f8a634ae2b4d29
Перминов Сергей Олегович <S_Perminov@bz.ru>
2023-03-23 15:37:39 +0300

    task5 some minor fix

--- task5/company/src/main/java/controller/AppController.java
+++ task5/company/src/main/java/controller/AppController.java
@@ -1,14 +1,15 @@
 package controller;
 
+import config.AppConfig;
 import view.menu.impl.GeneralMenuImpl;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class AppController {
-    public static final Logger LOGGER = Logger.getLogger(AppController.class.getName());
+
     public static void start(){
-        LOGGER.log(Level.INFO, "Application started");
+        AppConfig.LOGGER.log(Level.INFO, "Application started");
 
         new GeneralMenuImpl().processing();
     }
